6ef21ec45e4efa71cbc069a2e36ab835a57d52b7
Wish <wish6424@hotmail.com>
2023-04-12 13:24:22 +0700

    add travelMethod to parameters

--- nimbusCore/mcts_algo/nimbus_algo_v3r.py
+++ nimbusCore/mcts_algo/nimbus_algo_v3r.py
@@ -16,7 +16,7 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__)))
 from helper import timeStringToTime
 
 
-def generatePlan(places, tags, distanceMatrix, walkMatrix, userSelectedTags, budget):
+def generatePlan(places, tags, distanceMatrix, walkMatrix, userSelectedTags, budget, travelMethod):
     # deep copy to avoid overwriting variables
     distanceMatrix = copy.deepcopy(distanceMatrix)
     walkMatrix = copy.deepcopy(walkMatrix)
@@ -82,45 +82,58 @@ def generatePlan(places, tags, distanceMatrix, walkMatrix, userSelectedTags, bud
     
     # SCORING FUNCTION: tagMatched + rating / distance
     placeScoresMatrix = {}
-    travelTypeMatrix = {}
+    travelMethodMatrix = {}
     for x in places:
         placeScoresMatrix[x['loc_id']] = {}
-        travelTypeMatrix[x['loc_id']] = {}
+        travelMethodMatrix[x['loc_id']] = {}
 
         for y in places:
             if 'wait' in x['tags'] or 'wait' in y['tags'] or x['loc_id'] == y['loc_id']:
                 placeScoresMatrix[x['loc_id']][y['loc_id']] = 0
-                travelTypeMatrix[x['loc_id']][y['loc_id']] = 'none'
+                travelMethodMatrix[x['loc_id']][y['loc_id']] = 'none'
             else:
                 walkTime = walkMatrix[x['loc_id']][y['loc_id']].seconds
                 driveTime = walkMatrix[x['loc_id']][y['loc_id']].seconds
-                # driveScore = 0.99 * placeScores[y['loc_id']] / ((1 + driveTime) ** 0.5) # score if drive
-                driveScore = placeScores[y['loc_id']] / ((1 + driveTime)) # original
+                driveScore = placeScores[y['loc_id']] / ((1 + driveTime)) # v1.0
+                # driveScore = 0.99 * placeScores[y['loc_id']] / ((1 + driveTime) ** 0.5) # v1.1 :(
                 placeScoresMatrix[x['loc_id']][y['loc_id']] = driveScore
-                if walkTime <= 15 * 60: # set travelType
-                    travelTypeMatrix[x['loc_id']][y['loc_id']] = 'walk'
-                else:
-                    travelTypeMatrix[x['loc_id']][y['loc_id']] = 'drive'
+                
+                # travel method
+                # 1) walk and drive 
+                if 'walk' in travelMethod and 'drive' in travelMethod:
+                    if walkTime <= 15 * 60 and 'walk': # walk if less than 15 mins
+                        travelMethodMatrix[x['loc_id']][y['loc_id']] = 'walk'
+                    else:
+                        travelMethodMatrix[x['loc_id']][y['loc_id']] = 'drive'
+                # 2) walk only
+                elif 'walk' in travelMethod:
+                    travelMethodMatrix[x['loc_id']][y['loc_id']] = 'walk'
+                # 3) drive only
+                elif 'drive' in travelMethod:
+                    if walkTime <= 5 * 60 and 'walk': # walk if less than 15 mins
+                        travelMethodMatrix[x['loc_id']][y['loc_id']] = 'walk'
+                    else:
+                        travelMethodMatrix[x['loc_id']][y['loc_id']] = 'drive'
 
     placeScoresMatrix['start'] = {place['loc_id'] : 0 for place in places} # start score matrix = 0 to treat every node equally
-    travelTypeMatrix['start'] = {place['loc_id'] : 'none' for place in places}
+    travelMethodMatrix['start'] = {place['loc_id'] : 'none' for place in places}
 
-    # print(travelTypeMatrix)
+    # print(travelMethodMatrix)
 
-    def getTravelDuration(x, y, travelType):
+    def getTravelDuration(x, y, travelMethod):
         if x == y or 'wait' in [x, y] or 'start' in [x, y]:
             return datetime.timedelta() # 0
 
-        if travelType == 'walk':
+        if travelMethod == 'walk':
             return walkMatrix[x][y]
         
         return distanceMatrix[x][y]
 
-    def getTravelType(x, y):
+    def getTravelMethod(x, y):
         if x == y or 'wait' in [x, y] or 'start' in [x, y]:
             return 'none'
 
-        return travelTypeMatrix[x][y]
+        return travelMethodMatrix[x][y]
     
     # # MCTS ALGORITHM
     class Node:
@@ -136,13 +149,13 @@ def generatePlan(places, tags, distanceMatrix, walkMatrix, userSelectedTags, bud
             # self.nodeReward = None # unused
 
             if parent is not None:
-                self.travelType = getTravelType(self.loc_id, parent.loc_id)  # method to get here
-                self.travelDuration = getTravelDuration(self.loc_id, parent.loc_id, self.travelType)  # time to get here
+                self.travelMethod = getTravelMethod(self.loc_id, parent.loc_id)  # method to get here
+                self.travelDuration = getTravelDuration(self.loc_id, parent.loc_id, self.travelMethod)  # time to get here
                 self.arrivalTime = parent.leaveTime + self.travelDuration
                 self.leaveTime = parent.leaveTime + self.travelDuration + self.est_time_stay
             else: # first place
+                self.travelMethod = 'none' # method to get here
                 self.travelDuration = 0
-                self.travelType = 'none' # method to get here
                 self.arrivalTime = startHour
                 self.leaveTime = startHour + self.est_time_stay
 
@@ -218,14 +231,14 @@ def generatePlan(places, tags, distanceMatrix, walkMatrix, userSelectedTags, bud
             # append travel duration
             itArr.append({'type': 'travel_dur',
                         'travel_dur': pointer.travelDuration.seconds,
-                        'travel_type': pointer.travelType,
+                        'travel_type': pointer.travelMethod,
                         })
             # append next location
             itArr.append({'type': 'locations',
                         'loc_id': pointer.loc_id,
                         'arrival_time': pointer.arrivalTime.time().isoformat(),
                         'leave_time': pointer.leaveTime.time().isoformat(),
-                        'reward': pointer.totalReward / pointer.visitCount,
+                        # 'reward': pointer.totalReward / pointer.visitCount,
                         # 'totalReward': pointer.totalReward,
                         # 'visitCount': pointer.visitCount,
                         })
@@ -262,11 +275,11 @@ def generatePlan(places, tags, distanceMatrix, walkMatrix, userSelectedTags, bud
                             # money budget
                             and isLowerThanBudget(place, selectedPlace, budget)
                             # time budget
-                            and pointer.leaveTime + getTravelDuration(pointer.loc_id, place['loc_id'], pointer.travelType) + place['est_time_stay'] < endHour
+                            and pointer.leaveTime + getTravelDuration(pointer.loc_id, place['loc_id'], pointer.travelMethod) + place['est_time_stay'] < endHour
                             # after open hour
-                            and (pointer.leaveTime + getTravelDuration(pointer.loc_id, place['loc_id'], pointer.travelType)).time() >= place['hours'][0]
+                            and (pointer.leaveTime + getTravelDuration(pointer.loc_id, place['loc_id'], pointer.travelMethod)).time() >= place['hours'][0]
                             # before close hour
-                            and (pointer.leaveTime + getTravelDuration(pointer.loc_id, place['loc_id'], pointer.travelType) + place['est_time_stay']).time() <= place['hours'][1]
+                            and (pointer.leaveTime + getTravelDuration(pointer.loc_id, place['loc_id'], pointer.travelMethod) + place['est_time_stay']).time() <= place['hours'][1]
                             # food or not
                             and takeFood(place, pointer, hadFood)
                             ]
@@ -407,6 +420,10 @@ if __name__ == '__main__':
     # generate random userSelectedTags params
     userSelectedTags = [tag for tag in tags if randInt(1)]
     budget = 3
+    travelMethod = [
+                    'drive', 
+                    'walk'
+                    ]
     
     # TODO change parameters to dict object
     # TEST RUN
@@ -418,7 +435,8 @@ if __name__ == '__main__':
                        distanceMatrix=distanceMatrix,  
                        walkMatrix=walkMatrix,
                        userSelectedTags=userSelectedTags, 
-                       budget=budget)
+                       budget=budget,
+                       travelMethod=travelMethod)
     
     print(plan)
     print(userSelectedTags)
